0696aeb6b937d00d42379d4f66cb6f6d7931f99a
sultampan <sultansyarif630@gmail.com>
2024-12-02 11:34:52 +0800

    commit

---
+++ prestasikel1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class prestasikel1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nama, tingkatPrestasi;
+        int nim, jenis, tahun;
+        System.out.println("______________________________________________");
+        System.out.println("========PENCATATAN PRESTASI MAHASISWA========");
+        System.out.println("______________________________________________");
+        System.out.println("1. Tambah data prestasi");
+        System.out.println("2. Tampilkan semua prestasi");
+        System.out.println("3. Analisis prestasi berdasarkan jenis");
+        System.out.println("4. Keluar");
+        System.out.println("_______________________________________________");
+        System.out.print("Pilih menu: ");
+        int pilihanMenu = input.nextInt();
+        input.nextLine();
+
+    }
+}
+    
